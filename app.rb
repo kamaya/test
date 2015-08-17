fa10048d23ef3b4853ea38b056de53371a6311b4
kamaya <r.k.y.g.7.0813@gmail.com>
2015-08-17 06:04:47 +0000

    before/afterフィルタ

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra'
 
+before '/admin/*' do
+  @msg="admin area!"
+end
+
+before do
+  @author="taguchi"
+end
+
+after do
+  logger.info "page displayed successfullly"
+end
+
 get '/' do
   @title="main index"
-  @content="main content"
+  @content="main content by " + @author
   erb :index
 end
 
 get '/about' do
   @title="about this page"
-  @content="this page is ..."
+  @content="this page is ... by " + @author
   @email="taguchi@mail"
   erb :about
 end

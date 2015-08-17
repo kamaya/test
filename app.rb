189a4357712ea7c64a9ca1942834450dc4f4927e
kamaya <r.k.y.g.7.0813@gmail.com>
2015-08-17 06:11:56 +0000

    helperを使う

--- app.rb
+++ app.rb
@@ -12,6 +12,12 @@ after do
   logger.info "page displayed successfullly"
 end
 
+helpers do
+  def strong(s)
+    "<strong>#{s}</strong>"
+  end
+end
+  
 get '/' do
   @title="main index"
   @content="main content by " + @author
@@ -20,7 +26,7 @@ end
 
 get '/about' do
   @title="about this page"
-  @content="this page is ... by " + @author
+  @content="this page is ... by " + strong(@author)
   @email="taguchi@mail"
   erb :about
 end

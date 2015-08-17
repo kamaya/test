4c09b130aeb5cae1fab90b07aa02801bf913e461
kamaya <r.k.y.g.7.0813@gmail.com>
2015-08-17 05:51:13 +0000

    layout.erbを使う

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
-get '/:name' do |n|
-  @name=n;
+get '/' do
+  @title="main index"
+  @content="main content"
   erb :index
+end
+
+get '/about' do
+  @title="about this page"
+  @content="this page is ..."
+  @email="taguchi@mail"
+  erb :about
 end
